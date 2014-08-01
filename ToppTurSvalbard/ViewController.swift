a2454dd32eb55eda6c63781a6db7ed420751a64c
Kjetil Dahl Knutsen <kjetildk@jok-lt.ksat.ksat.no>
2014-08-01 20:12:30 +0200

    Initial Commit

---
+++ ToppTurSvalbard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToppTurSvalbard
+//
+//  Created by Kjetil Dahl Knutsen on 01.08.14.
+//  Copyright (c) 2014 publisoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
